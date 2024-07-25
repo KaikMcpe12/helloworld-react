559d7492f90ed0a596d93c676bfe79b9006ee269
Kaik <119709479+KaikMcpe12@users.noreply.github.com>
2024-07-25 09:28:46 -0300

    :tada: feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      <h1>Hello World - React</h1>
+      <p>Simples programa para testar uma aplicação React</p>
+    </>
+  )
+}
+
+export default App
